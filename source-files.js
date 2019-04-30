5375a2d5fcdb2f7705da61922eb0f2ff07bf3d20
Travis CI User <travis@example.org>
2019-04-30 02:04:21 +0000

    Documentation for Kimundi/rustc-version-rs@0294f2b

---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rustc_version"] = {"name":"","dirs":[],"files":["errors.rs","lib.rs"]};
+sourcesIndex["semver"] = {"name":"","dirs":[],"files":["lib.rs","version.rs","version_req.rs"]};
+sourcesIndex["semver_parser"] = {"name":"","dirs":[],"files":["common.rs","lib.rs","range.rs","recognize.rs","version.rs"]};
+createSourceSidebar();
